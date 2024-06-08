dc2ae93823d7d350feeb2e5f18190a9c3823b7b4
AquaFrostByte <96489431+AquaFrostByte@users.noreply.github.com>
2024-06-08 18:42:34 +0200

    Dont Run it K?
    Its an Ram filler right now with 40runs:: so about to crash 30GB Ram or Lag out an 32GB ram maschine

---
+++ NUMPY_V2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import threading
+while True:
+  def process_array(data):
+    return data * 20000 
+  def process_data_threaded(data_list):
+    threads = []
+    results = []
+    for data in data_list:
+      result = threading.Thread(target=process_array, args=(data.copy(),))
+      threads.append(result)
+      result.start()
+      results.append(result)
+    for result in results:
+      result.join()
+  if __name__ == "__main__":
+    data_size = 1024 * 1024 * 1024
+    num_arrays = 40
+    data_list = []
+    for _ in range(num_arrays):
+      data = np.full(data_size, 255, dtype=np.uint8)
+      data_list.append(data)
+
+
+
