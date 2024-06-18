297e8250cf4f3016ae9a209f128aa46a56861de4
AquaFrostByte <96489431+AquaFrostByte@users.noreply.github.com>
2024-06-18 21:38:22 +0200

    Add files via upload
    it has a name finaly RAMY == Ram in cool :3. now real with V3 you can finaly stop the program and it prints the Array so if its not printing it right something is not good.

---
+++ Ramy_V3_PennTest.py
@@ -0,0 +1,28 @@
+import numpy as np
+import threading
+import keyboard
+
+num_arrays = int(input("how much ram you want to test?")) + 10
+print("HOLD Q TO STOP!")
+input("Start? Press Enter")
+while not keyboard.is_pressed('Q'):
+  def process_array(data):
+    return data * 20000 
+  def process_data_threaded(data_list):
+    threads = []
+    results = []
+    for data in data_list:
+      result = threading.Thread(target=process_array, args=(data.copy(),))
+      threads.append(result)
+      result.start()
+      results.append(result)
+      print(result)
+    for result in results:
+      result.join()
+  if __name__ == "__main__":
+    data_size = 1024 * 1024 * 1024
+    data_list = []
+    for _ in range(num_arrays):
+      data = np.full(data_size, 255, dtype=np.uint8)
+      data_list.append(data)
+  print(data)
